e5747753b746a34aa332d2d226426a424e83bbef
ahmed777ismail <ahmed1kaball@gmail.com>
2025-05-07 16:54:12 +0300

    feat: Add main layout structure and partials for front-end views
    - Created master layout file with structure for header, footer, sidebar, and scripts.
    - Added footer partial with copyright and links.
    - Implemented head partial for meta tags, styles, and favicon.
    - Developed header partial with navigation and user dropdown.
    - Included scripts partial for core and vendor JavaScript files.
    - Established sidebar partial with navigation links and icons for various pages and components.

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\FrontHomeController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
@@ -13,6 +14,10 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
+
+Route::prefix('front')->name('front.')->group(function () {
+    Route::get('/', FrontHomeController::class)->name('index');
+});
 
 Route::get('/', function () {
     return view('welcome');
@@ -28,4 +33,4 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
