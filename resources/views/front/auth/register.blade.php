69f6ca8caf0ebbbea2715238c041626b25d26ab7
ahmed777ismail <ahmed1kaball@gmail.com>
2025-05-14 16:29:05 +0300

    feat: Update registration form and routing for front-end authentication

--- resources/views/front/auth/register.blade.php
+++ resources/views/front/auth/register.blade.php
@@ -25,21 +25,25 @@
               <h4 class="mb-2">Adventure starts here 🚀</h4>
               <p class="mb-4">Make your app management easy and fun!</p>
 
-              <form id="formAuthentication" class="mb-3" action="index.html" method="POST">
+              <form id="formAuthentication" class="mb-3" action="{{ route('register') }}" method="POST">
+                 @csrf
                 <div class="mb-3">
-                  <label for="username" class="form-label">Username</label>
+                  <label for="username" class="form-label">Name</label>
                   <input
                     type="text"
                     class="form-control"
                     id="username"
-                    name="username"
+                    name="name"
                     placeholder="Enter your username"
                     autofocus
+                    :value="old('name')"
                   />
+                    <x-input-error :messages="$errors->get('name')" class="mt-2" />
                 </div>
                 <div class="mb-3">
                   <label for="email" class="form-label">Email</label>
-                  <input type="text" class="form-control" id="email" name="email" placeholder="Enter your email" />
+                  <input type="text" class="form-control" id="email" name="email" placeholder="Enter your email" :value="old('email')"/>
+                   <x-input-error :messages="$errors->get('email')" class="mt-2" />
                 </div>
                 <div class="mb-3 form-password-toggle">
                   <label class="form-label" for="password">Password</label>
@@ -54,9 +58,29 @@
                     />
                     <span class="input-group-text cursor-pointer"><i class="bx bx-hide"></i></span>
                   </div>
+                      <x-input-error :messages="$errors->get('password')" class="mt-2" />
                 </div>
 
-                <div class="mb-3">
+
+
+
+             <div class="mb-3 form-password-toggle">
+                  <label class="form-label" for="password">Password Confirmation</label>
+                  <div class="input-group input-group-merge">
+                    <input
+                      type="password"
+                      id="password"
+                      class="form-control"
+                      name="password_confirmation"
+                      placeholder="&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;"
+                      aria-describedby="password"
+                    />
+                    <span class="input-group-text cursor-pointer"><i class="bx bx-hide"></i></span>
+                  </div>
+                      <x-input-error :messages="$errors->get('password_confirmation')" class="mt-2" />
+                </div>
+
+                {{-- <div class="mb-3">
                   <div class="form-check">
                     <input class="form-check-input" type="checkbox" id="terms-conditions" name="terms" />
                     <label class="form-check-label" for="terms-conditions">
@@ -64,7 +88,8 @@
                       <a href="javascript:void(0);">privacy policy & terms</a>
                     </label>
                   </div>
-                </div>
+                </div> --}}
+
                 <button class="btn btn-primary d-grid w-100">Sign up</button>
               </form>
 
--- routes/web.php
+++ routes/web.php
@@ -4,36 +4,26 @@ use App\Http\Controllers\FrontHomeController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
 
 Route::prefix('front')->name('front.')->group(function () {
-    Route::get('/', FrontHomeController::class)->name('index');
+    Route::get('/', FrontHomeController::class)->middleware('auth')->name('index');
     Route::view('/login', 'front.auth.login');
     Route::view('/register', 'front.auth.register');
     Route::view('/forgot-password', 'front.auth.forgot-password');
 });
+require __DIR__ . '/auth.php';
+
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+// Route::get('/dashboard', function () {
+//     return view('dashboard');
+// })->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-});
-
-require __DIR__ . '/auth.php';
+// Route::middleware('auth')->group(function () {
+//     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+//     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+//     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+// });
